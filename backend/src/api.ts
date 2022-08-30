95297a39cfcc26304ec9446c62e4cd7610817185
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2022-08-29 21:15:20 -0400

    change: multi-row insert post method

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import { getAll, insert } from './db';
-import { dbAdmin, dbStudensts, dbTeachers, dbSections, dbClasses, dbSemesters, dbProfession, dbShifts, dbRoles, dbPersons } from './types';
+import { getAll, insertSingle, insertMultiple } from './db';
+import { dbAdmin, dbStudensts, dbTeachers, dbSections, dbClasses, dbSemesters, dbProfession, dbShifts, dbRoles, dbPersons, dbPensum } from './types';
 
 const router = express.Router();
 
@@ -17,7 +17,7 @@ router.get('/classes', (req,res)=>{
 router.post('/classes', (req,res)=>{
     const valida = ((object: any):object is dbClasses => 'names' in object)(req.body);
     if(valida){
-        insert('classes',req.body)
+        insertSingle('classes',req.body)
             .then(()=>res.status(200).json({msg:'Materia agregada'}))
             .catch(err=>console.log(err));
     }else{
@@ -26,14 +26,14 @@ router.post('/classes', (req,res)=>{
 });
 
 // RUTA PARA LOS SEMESTRES
-router.get('/semesters', async(req,res)=>{
+router.get('/semesters', (req,res)=>{
     getAll('semesters').then(resolve=>res.status(200).json(resolve))
     .catch(err=>console.log(err));
 });
 router.post('/semesters', (req,res)=>{
     const valida = ((object: any):object is dbSemesters => 'names' in object)(req.body);
     if(valida){
-        insert('semesters',req.body)
+        insertSingle('semesters',req.body)
             .then(()=>res.status(200).json({msg:'Semestre agregado'}))
             .catch(err=>console.log(err));
     }else{
@@ -42,14 +42,14 @@ router.post('/semesters', (req,res)=>{
 });
 
 // RUTAS PARA LAS PROFESIONES
-router.get('/profession', async(req,res)=>{
+router.get('/profession', (req,res)=>{
     getAll('profession').then(resolve=>res.status(200).json(resolve))
     .catch(err=>console.log(err));
 });
 router.post('/profession', (req,res)=>{
     const valida = ((object: any):object is dbProfession => 'names' in object)(req.body);
     if(valida){
-        insert('profession',req.body)
+        insertSingle('profession',req.body)
             .then(()=>res.status(200).json({msg:'Profesion agregada'}))
             .catch(err=>console.log(err));
     }else{
@@ -58,14 +58,14 @@ router.post('/profession', (req,res)=>{
 });
 
 //RUTAS PARA LOS TURNOS
-router.get('/shifts', async(req,res)=>{
+router.get('/shifts', (req,res)=>{
     getAll('shifts').then(resolve=>res.status(200).json(resolve))
     .catch(err=>console.log(err));
 });
 router.post('/shifts', (req,res)=>{
     const valida = ((object: any):object is dbShifts => 'names' in object)(req.body);
     if(valida){
-        insert('shifts',req.body)
+        insertSingle('shifts',req.body)
             .then(()=>res.status(200).json({msg:'Turno agregado'}))
             .catch(err=>console.log(err));
     }else{
@@ -74,14 +74,14 @@ router.post('/shifts', (req,res)=>{
 });
 
 //RUTAS PARA LAS SECCIONES
-router.get('/sections', async(req,res)=>{
+router.get('/sections', (req,res)=>{
     getAll('sections').then(resolve=>res.status(200).json(resolve))
     .catch(err=>console.log(err));
 });
-router.post('/sections', async(req,res)=>{
+router.post('/sections', (req,res)=>{
     const valida = ((object: any):object is dbSections => 'names' in object)(req.body);
     if(valida){
-        insert('sections',req.body)
+        insertSingle('sections',req.body)
             .then(()=>res.status(200).json({msg:'Seccion agregada'}))
             .catch(err=>console.log(err));
     }else{
@@ -90,14 +90,14 @@ router.post('/sections', async(req,res)=>{
 });
 
 //RUTAS PARA LOS ROLES
-router.get('/roles', async(req,res)=>{
+router.get('/roles', (req,res)=>{
     getAll('roles').then(resolve=>res.status(200).json(resolve))
     .catch(err=>console.log(err));
 });
-router.post('/roles', async(req,res)=>{
+router.post('/roles', (req,res)=>{
     const valida = ((object: any):object is dbRoles => 'names' in object)(req.body);
     if(valida){
-        insert('roles',req.body)
+        insertSingle('roles',req.body)
             .then(()=>res.status(200).json({msg:'Rol agregado'}))
             .catch(err=>console.log(err));
     }else{
@@ -107,11 +107,11 @@ router.post('/roles', async(req,res)=>{
 
 
 // RUTAS PARA LAS PERSONAS
-router.get('/persons', async(req,res)=>{
+router.get('/persons', (req,res)=>{
     getAll('persons').then(resolve=>res.status(200).json(resolve))
     .catch(err=>console.log(err));
 });
-router.post('/persons', async(req,res)=>{
+router.post('/persons', (req,res)=>{
     const valida = ((object: any):object is dbPersons => {
         return 'names' in object &&
         'lastNames' in object &&
@@ -122,7 +122,7 @@ router.post('/persons', async(req,res)=>{
         'role' in object;
     })(req.body);
     if(valida){
-        insert('persons',req.body)
+        insertSingle('persons',req.body)
             .then(()=>res.status(200).json({msg:'Persona agregada'}))
             .catch(err=>console.log(err));
     }else{
@@ -132,13 +132,14 @@ router.post('/persons', async(req,res)=>{
 
 
 // RUTA PARA LOGIN
-router.get('/login', async(req,res)=>{
+router.get('/login', (req,res)=>{
     getAll('login').then(resolve=>res.status(200).json(resolve))
     .catch(err=>console.log(err));
 });
 
+
 //RUTAS PARA LOS PROFESORES
-router.post('/teachers', async(req,res)=>{
+router.post('/teachers', (req,res)=>{
     const valida = ((object: any):object is dbTeachers => {
         return 'IdPersons' in object &&
             "IdProfession" in object &&
@@ -148,7 +149,7 @@ router.post('/teachers', async(req,res)=>{
             "IdSections" in object;
     })(req.body);
     if(valida){
-        insert('teachers',req.body)
+        insertSingle('teachers',req.body)
             .then(()=>res.status(200).json({msg:'Profesor agregado'}))
             .catch(err=>console.log(err));
     }else{
@@ -157,14 +158,14 @@ router.post('/teachers', async(req,res)=>{
 });
 
 //RUTAS PARA LOS ESTUDIANTES
-router.post('/students', async(req,res)=>{
+router.post('/students', (req,res)=>{
     const valida = ((object: any):object is dbStudensts => {
         return 'IdPersons' in object &&
             "IdProfession" in object &&
             "IdSemesters" in object;
     })(req.body);
     if(valida){
-        insert('students',req.body)
+        insertSingle('students',req.body)
             .then(()=>res.status(200).json({msg:'Estudiante agregado'}))
             .catch(err=>console.log(err));
     }else{
@@ -173,10 +174,10 @@ router.post('/students', async(req,res)=>{
 });
 
 //RUTAS PARA LOS ADMINISTRADORES
-router.post('/admin', async(req,res)=>{
+router.post('/admin', (req,res)=>{
     const valida = ((object: any):object is dbAdmin => 'IdPersons' in object)(req.body);
     if(valida){
-        insert('admin',req.body)
+        insertSingle('admin',req.body)
             .then(()=>res.status(200).json({msg:'Admin agregado'}))
             .catch(err=>console.log(err));
     }else{
@@ -184,6 +185,34 @@ router.post('/admin', async(req,res)=>{
     }
 });
 
+
+//RUTAS PARA PENSUM
+router.get('/pensum/:profession', (req,res)=>{
+    console.log(req.params);
+    res.status(200).send();
+    // getAll('pensum')
+    //     .then(resolve=>res.status(200).json(resolve))
+    //     .catch(err=>console.log(err));
+});
+router.post('/pensum', (req, res)=>{
+    const valida = ((objects: any[]):objects is dbPensum[] => {
+    let r = true;
+    for (let index = 0; index < objects.length; index++) {
+        if(!('IdProfession' in objects[index]) || !('IdSemesters' in objects[index]) || !('IdClasses' in objects[index])){
+            r = false;
+            break;
+        }
+    }
+    return r;
+    })(req.body);
+    if(valida){
+        insertMultiple('pensum',req.body)
+            .then(()=>res.status(200).json({msg:'Pensum agregado'}))
+            .catch(err=>console.log(err));
+    }else{
+        res.status(400).json({msg:"Error en la estructura de datos"});
+    }
+});
 
 
 
--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,6 +1,14 @@
 import { MysqlError, PoolConnection } from 'mysql';
 import { conn } from './conect';
 
+// La siguiente funcion, tranforma un arreglo de objesto en matriz.
+// Ejemplo [ { "valor1":1, "valor2":2 } , {"valor1":3, "valor2":4} ]  ->  [ [1,2] , [3,4] ]
+const transformData = (datas:[]):Array<number[]> => {
+    return datas.map(data => {
+        return Object.keys(data).map(key=>data[key]);
+    });
+}
+
 export const getAll = (table:string) => {
     return new Promise((resolve, reject)=>{
         conn.getConnection((MySqlErr:MysqlError,connection:PoolConnection)=>{
@@ -14,11 +22,25 @@ export const getAll = (table:string) => {
     });
 }
 
-export const insert = (table,data) => {
+export const insertSingle = (table,data) => {
     return new Promise((resolve, reject)=>{
         conn.getConnection((MySqlErr:MysqlError,connection:PoolConnection)=>{
             if(MySqlErr) reject(`Error al conectar a MySQL: ${MySqlErr}`);
             connection.query(`INSERT INTO ${table} set ?`, data, (QueryErr,result)=>{
+                if(QueryErr) reject( `Error en consulta a tabla ${table}: ${QueryErr}`);
+                if(result) resolve(true);
+                connection.release();
+            });
+        });
+    });
+}
+
+export const insertMultiple = (table:string,datas:[]) => {
+    return new Promise((resolve, reject)=>{
+        conn.getConnection((MySqlErr:MysqlError,connection:PoolConnection)=>{
+            if(MySqlErr) reject(`Error al conectar a MySQL: ${MySqlErr}`);
+            const newData = transformData(datas);
+            connection.query(`INSERT INTO ${table} (IdProfession,IdSemesters,IdClasses) VALUES ?`, [newData],(QueryErr,result)=>{
                 if(QueryErr) reject( `Error en consulta a tabla ${table}: ${QueryErr}`);
                 if(result) resolve(true);
                 connection.release();
