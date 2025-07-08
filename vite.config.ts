27b1f21b671faba2e54f670ce2bb36d3248a04a9
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2025-07-08 18:47:51 -0400

    fix: agrega tailwindcss a vite.config, se elimino por error

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import tsconfigPaths from "vite-tsconfig-paths"
 import compression from "vite-plugin-compression"
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     tsconfigPaths(),
     compression({
       algorithm: "brotliCompress",
